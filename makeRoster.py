f049e1d995e2588702eb0e10a7cd621173d85c2f
Terrific5454 <wangzhehao66@gmail.com>
2021-01-29 14:18:02 +0800

    加入名单去重

---
+++ makeRoster.py
@@ -0,0 +1,23 @@
+import json
+
+# 去重list变量
+existed = []
+
+# 写入用户名
+def writeName(roster, js):
+    username = js['username']
+    with open(roster, 'a+', encoding='utf-8') as file:
+        if username not in existed:
+            existed.append(json)
+            file.writelines(username)
+            file.writelines('\n')
+
+# 提取并生成名单
+def extractUname(raw, roster):
+    with open(raw, 'r', encoding='utf-8') as file:
+        while True:
+            line = file.readline()
+            if not line:  # 到 EOF，返回空字符串，则终止循环
+                break
+            js = json.loads(line)
+            writeName(roster, js)
