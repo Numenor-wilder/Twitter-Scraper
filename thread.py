125c9709c7ce0a424618a2b7af5a191a13b36e79
Terrific5454 <wangzhehao66@gmail.com>
2021-01-29 19:54:48 +0800

    重新封装，增加单用户查询方法，通过mention爬取测试

---
+++ thread.py
@@ -0,0 +1,21 @@
+import twint
+import datetime
+
+import config
+import makeRoster
+
+TIMEFORMAT = '%Y%m%d%H%M%S'
+
+
+# 生成目标名单
+def makeList(raw, roster):
+    makeRoster.extractUname(raw, roster)
+
+
+# 按username爬取profile
+def getProfile(roster):
+    with open(roster, 'r', encoding = 'utf-8') as file:
+        theTime = datetime.datetime.now().strftime(TIMEFORMAT)
+        for name in file:
+            config.configUname(name.strip(), theTime)
+            twint.run.Profile(config.config)
